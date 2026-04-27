dde473151a6c172da5ad471d0ceeedab0606ca7d
sangieb <u20242229207@usco.edu.co>
2026-04-27 17:28:43 -0500

    Proyecto Inicial: Spring Initializer

---
+++ src/test/java/com/parqueadero/parqueadero/ParqueaderoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parqueadero.parqueadero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueaderoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
